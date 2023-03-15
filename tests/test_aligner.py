8dd2000c3d50ca205f00b3906ca0710fc4d5a6d9
thuc-moreh <hoang.thuc@moreh.com.vn>
2023-03-15 01:53:31 +0000

    finish test

--- tests/test_aligner.py
+++ tests/test_aligner.py
@@ -1,4 +1,5 @@
-from aligner import Aligner
+import numpy as np
+from aligner import AlignResult, Aligner
 from transcriber import TranscribeResult
 
 
@@ -6,11 +7,19 @@ def test_aligner():
     LANGUAGE = "en"
     AUDIO_FILE = "tests/test_data/jfk.flac"
     TRUE_TRANSCRIBE_PATH = "tests/test_data/jfk_transcribe_result.json"
+    TRUE_ALIGN_PATH = "tests/test_data/jfk_align_result.json"
 
     aligner = Aligner(language=LANGUAGE)
 
     transcribe_result = TranscribeResult.from_json(TRUE_TRANSCRIBE_PATH)
 
     result = aligner.run(audio_file=AUDIO_FILE, transcribe_result=transcribe_result)
+    expected_result = AlignResult.from_json(TRUE_ALIGN_PATH)
+
+    for segment, expected_segment in zip(result.word_segments, expected_result.word_segments):
+        start = segment.start
+        expected_start = expected_segment.start
+
+        assert np.isclose(start, expected_start)
 
     assert True
